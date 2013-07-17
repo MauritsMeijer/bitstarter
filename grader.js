7d688d16db68b84269848256dbe1bbbda582e932
Maurits on EC2 <ubuntu@ip-10-204-131-226.ec2.internal>
2013-07-17 14:16:10 +0000

    bla

--- grader.js
+++ grader.js
@@ -26,7 +26,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var rest = require('./restler');
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -82,7 +82,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)        
-        .option('-u, --url <url>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to index.html', HTMLFILE_DEFAULT)
         .parse(process.argv);
     if(program.file){
         var checkJson = checkHtmlFile(program.file, program.checks);
